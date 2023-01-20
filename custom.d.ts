b029067bc85b0fda27656c82bae926cc367444c8
Paula Lavalle <768965+paulathevalley@users.noreply.github.com>
2023-01-19 17:58:05 -0800

    .svg module is also declared in astro/client. Add only ReactComponent for svgr plugin support. Add more compiler options to tsconfig and use strict.

--- custom.d.ts
+++ custom.d.ts
@@ -1,20 +1,14 @@
-type UswdsModule = {
-  on: (ele?: HTMLElement) => void;
-  off: (ele: HTMLElement) => void;
-};
-
 declare module "@uswds/uswds/js/*" {
-  const module: UswdsModule;
+  const module: {
+    on: (ele?: HTMLElement) => void;
+    off: (ele: HTMLElement) => void;
+  };
 
   export default module;
 }
 
-declare module '*.svg' {
-  /**
-   * Use `any` to avoid conflicts with
-   * `@svgr/webpack` plugin or
-   * `babel-plugin-inline-react-svg` plugin.
-   */
-  const content: string;
-  export default content;
+declare module "*.svg" {
+  // Add support for svgr plugin
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
 }
