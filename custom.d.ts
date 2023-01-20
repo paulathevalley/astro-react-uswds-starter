f9428dc9c4487c1b91fa4cf6cb51f6b780ca0103
Paula Lavalle <768965+paulathevalley@users.noreply.github.com>
2023-01-19 17:41:34 -0800

    Add custom defs to tsconfig. Try to declare .svg module

---
+++ custom.d.ts
@@ -0,0 +1,24 @@
+type UswdsModule = {
+  on: (ele?: HTMLElement) => void
+  off: (ele: HTMLElement) => void
+}
+
+declare module '@uswds/uswds/js/*' {
+  const module: UswdsModule
+
+  export default module
+}
+
+
+declare module '*.svg' {
+  /**
+   * Use `any` to avoid conflicts with
+   * `@svgr/webpack` plugin or
+   * `babel-plugin-inline-react-svg` plugin.
+   */
+  export const ReactComponent: React.SVGAttributes<{}>
+  
+  const content: string
+  export default content
+}
+
