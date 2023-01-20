5b5e93f6242ef7d96fdbafba440495130e18ebeb
Paula Lavalle <768965+paulathevalley@users.noreply.github.com>
2023-01-19 17:34:42 -0800

    Add support for rendering SVG files as react components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url'
 
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import svgr from 'vite-plugin-svgr'
 
 // In ESM, __dirname is no longer available
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -12,6 +13,7 @@ export default defineConfig({
   integrations: [react()],
    // Pass additional configuration options to Vite: vitejs.dev
   vite: {
+    plugins: [svgr()],
     ssr: {
       // Importing CSS files from @uswds package with file extension omitted e.g. @uswds/styles
       noExternal: ['@uswds'],
