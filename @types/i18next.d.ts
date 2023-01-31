4798b041baf143745d0cfad08566c360a7e1f07d
Paula Lavalle <768965+paulathevalley@users.noreply.github.com>
2023-01-31 15:32:27 -0800

    update module declaration

--- @types/i18next.d.ts
+++ @types/i18next.d.ts
@@ -1,18 +1,19 @@
 // import the original type declarations
 import "i18next";
 // import all namespaces (for the default language, only)
-// import ns1 from "public/locales/en/ns1.json";
+import en from "public/locales/en/translation.json";
+import es from "public/locales/es/translation.json";
 
-// declare module "i18next" {
-//   // Extend CustomTypeOptions
-//   interface CustomTypeOptions {
-//     // custom namespace type, if you changed it
-//     defaultNS: "ns1";
-//     // custom resources type
-//     resources: {
-//       ns1: typeof ns1;
-//       ns2: typeof ns2;
-//     };
-//     // other
-//   }
-// }
+declare module "i18next" {
+  // Extend CustomTypeOptions
+  interface CustomTypeOptions {
+    // custom namespace type, if you changed it
+    defaultNS: "en";
+    // custom resources type
+    resources: {
+      en: typeof en;
+      es: typeof es;
+    };
+    // other
+  }
+}
