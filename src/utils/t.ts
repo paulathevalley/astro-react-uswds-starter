7ec7d2a8eadeb17a6ee72422a6f47d1bd9298725
Paula Lavalle <768965+paulathevalley@users.noreply.github.com>
2023-08-31 14:24:08 -0700

    Fix check error

--- src/utils/t.ts
+++ src/utils/t.ts
@@ -2,12 +2,13 @@ export const locales = {
   en: "en-US", // the `defaultLocale` value must present in `locales` keys
   es: "es-US",
 };
-
 // translations
 import enTranslations from "@locales/en/translation.json";
 import esTranslations from "@locales/es/translation.json";
 
-export function t(key: string, locale?: keyof typeof locales) {
+type Translations = typeof enTranslations | typeof esTranslations;
+
+export function t(key: keyof Translations, locale?: keyof typeof locales) {
   switch (locale) {
     case "es":
       if (esTranslations[key]) {
