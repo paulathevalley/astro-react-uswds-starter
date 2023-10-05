3c8a5510c17893dc7303a011f3a1355bf06569b5
Paula Lavalle <768965+paulathevalley@users.noreply.github.com>
2023-10-05 15:18:52 -0700

    Add i18next (#65)
    * Add i18next and use it
    
    * lint
    
    * prettier
    
    * fix images
    
    * lint
    
    * prettier
    
    * add subpage to check build
    
    * Add example with base path. Upgrade astro-i18n-aut to handle it correctly.
    
    * prettier

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,4 @@
 import { sequence } from "astro/middleware";
 import { i18nMiddleware } from "astro-i18n-aut";
 
-const i18n = i18nMiddleware({ defaultLocale: "en" });
-
-export const onRequest = sequence(i18n);
+export const onRequest = sequence(i18nMiddleware);
